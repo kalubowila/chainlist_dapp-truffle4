a2ac7731cd748ab147ed7e114a497c6a085a7a24
3LOGIXS\tharindu.kalubowila <tharindu.kalubowila@eyepax.com>
2018-11-22 12:00:19 +0530

    Adding frontend files to the gihub pages

--- truffle.js
+++ truffle.js
@@ -14,6 +14,14 @@ module.exports = {
                network_id: "4224",
                gas: 4700000,
                // from: '0x85b2c41e4c801ff408852a75a08939589d1b6eaf'
+          },
+
+          rinkeby: {
+               host: "http://127.0.0.1",
+               port: 8545,
+               network_id: "4", //rinkeby test network
+               gas: 5000000
+               // from: "0xbe572fe4102c5247713b154f0db83186e0315b1c"
           }
      }
 };
