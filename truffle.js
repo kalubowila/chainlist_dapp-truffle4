f39fcb29e6f596a8854b0008bd37deb10da10590
3LOGIXS\tharindu.kalubowila <tharindu.kalubowila@eyepax.com>
2018-11-22 21:05:07 +0530

    Adding frontend files to the gihub pages

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -19,9 +22,18 @@ module.exports = {
           rinkeby: {
                host: "http://127.0.0.1",
                port: 8545,
-               network_id: "4", //rinkeby test network
+               network_id: 4, //rinkeby test network
                gas: 5000000
                // from: "0xbe572fe4102c5247713b154f0db83186e0315b1c"
-          }
+          },
+
+          ropsten: {
+               provider: function () {
+                   return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY);
+               },
+               network_id: 3,
+               gas: 4500000,
+               gasPrice: 10000000000
+         }
      }
 };
